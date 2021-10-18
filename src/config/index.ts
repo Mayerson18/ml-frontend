548339d26b199081d80a40d2ba6692f8e79401d2
mayerson18 <mayerson18@gmail.com>
2021-10-18 10:52:25 -0500

    fix: add url heroku

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.API_URL || "http://localhost:3000",
+  API_URL: process.env.API_URL || "https://mayerson-ml-backend.herokuapp.com",
 }
 
 export default config;
